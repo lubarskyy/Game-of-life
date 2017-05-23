6032f2d523e55791db2c4ca9386c5c948f51235b
Robert Lubarski <lubarski.robert@gmail.com>
2017-05-23 17:12:42 +0200

    Added iteration counter, solved buttons problems

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 document.addEventListener('DOMContentLoaded', function(){
 
   function GameOfLife(){
+
     this.map = function(){
       this.width = document.querySelector('#fieldWidth').value / 10;
       this.height = document.querySelector('#fieldHeight').value / 10;
       var board = document.querySelector('#board');
       this.createBoard = function(){
+        while(board.hasChildNodes() == true){
+          board.removeChild(board.firstChild);
+        }
         board.style.width = this.width * 10 + 'px';
         board.style.height = this.height * 10 + 'px';
         var counter = this.width * this.height;
@@ -30,6 +34,7 @@ document.addEventListener('DOMContentLoaded', function(){
         }
       }
     }
+
     this.firstGlider = function (number){
       if(number == 1){
         this.setCellState(2, 0, 'live');
@@ -61,6 +66,7 @@ document.addEventListener('DOMContentLoaded', function(){
         this.setCellState(this.width / 2 - 3, this.height / 2 + 1, 'live');
       }
     }
+
     this.computeCellNextState = function(x, y){
       var counter = 0;
       var cellsArray = [
@@ -90,6 +96,7 @@ document.addEventListener('DOMContentLoaded', function(){
         return 0;
       }
     }
+
     var nextGenArray = [];
     var self = this;
     this.computeNextGeneration = function(){
@@ -100,9 +107,16 @@ document.addEventListener('DOMContentLoaded', function(){
         }
       }
     }
+
+    var counter = 0;
     this.printNextGeneration = function(){
       this.computeNextGeneration();
       this.idInterval = setInterval(function(){
+        counter++;
+        document.querySelector('.counter').innerText = counter;
+        if(nextGenArray.indexOf(1) == -1){
+          self.stopGame();
+        }
         for (var i = 0; i < self.cells.length; i++){
           if(nextGenArray[i] == 1){
             self.cells[i].classList.add('live');
@@ -113,46 +127,94 @@ document.addEventListener('DOMContentLoaded', function(){
         self.computeNextGeneration();
       }, 100);
     }
+
+    this.stopGame = function(){
+      clearInterval(self.idInterval);
+      document.querySelector('#map').disabled = false;
+      document.querySelector('#play').disabled = false;
+      document.querySelector('#shape1').disabled = false;
+      document.querySelector('#shape2').disabled = false;
+      document.querySelector('#shape3').disabled = false;
+      document.querySelector('#shape4').disabled = false;
+      counter = 0;
+    }
+
     document.querySelector('#play').addEventListener('click', function(){
+      document.querySelector('#map').disabled = true;
+      document.querySelector('#play').disabled = true;
+      document.querySelector('#shape1').disabled = true;
+      document.querySelector('#shape2').disabled = true;
+      document.querySelector('#shape3').disabled = true;
+      document.querySelector('#shape4').disabled = true;
       self.printNextGeneration();
     });
+
     document.querySelector('#pause').addEventListener('click', function(){
+      document.querySelector('#map').disabled = false;
+      document.querySelector('#play').disabled = false;
+      document.querySelector('#shape1').disabled = false;
+      document.querySelector('#shape2').disabled = false;
+      document.querySelector('#shape3').disabled = false;
+      document.querySelector('#shape4').disabled = false;
       clearInterval(self.idInterval);
     });
+
     document.querySelector('#map').addEventListener('click', function(){
       self.map();
       self.createBoard();
     });
+
     document.querySelector('#clear').addEventListener('click', function(){
+      counter = 0;
+      document.querySelector('.counter').innerText = counter;
+      document.querySelector('#map').disabled = false;
+      document.querySelector('#play').disabled = false;
+      document.querySelector('#shape1').disabled = false;
+      document.querySelector('#shape2').disabled = false;
+      document.querySelector('#shape3').disabled = false;
+      document.querySelector('#shape4').disabled = false;
       clearInterval(self.idInterval);
       self.cells.forEach(function(el){
         el.classList.remove('live');
       });
     });
+
     document.querySelector('#shape1').addEventListener('click', function(){
+      counter = 0;
+      document.querySelector('.counter').innerText = counter;
       self.cells.forEach(function(el){
         el.classList.remove('live');
       });
       self.firstGlider(1);
     });
+
     document.querySelector('#shape2').addEventListener('click', function(){
+      counter = 0;
+      document.querySelector('.counter').innerText = counter;
       self.cells.forEach(function(el){
         el.classList.remove('live');
       });
       self.firstGlider(2);
     });
+
     document.querySelector('#shape3').addEventListener('click', function(){
+      counter = 0;
+      document.querySelector('.counter').innerText = counter;
       self.cells.forEach(function(el){
         el.classList.remove('live');
       });
       self.firstGlider(3);
     });
+
     document.querySelector('#shape4').addEventListener('click', function(){
+      counter = 0;
+      document.querySelector('.counter').innerText = counter;
       self.cells.forEach(function(el){
         el.classList.remove('live');
       });
       self.firstGlider(4);
     });
+
   };
 
   var game = new GameOfLife();
